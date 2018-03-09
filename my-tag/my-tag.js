e4e817d72f3a5a3fd245a407dfdfa5fc213b9b9b
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2018-03-09 18:44:57 -0300

    Update my-tag.js

--- my-tag/my-tag.js
+++ my-tag/my-tag.js
@@ -1,19 +1,22 @@
 (function () {
 
-	const urlBase = 'https://murilloves.github.io/'
+	// const urlProd = 'https://murilloves.github.io/';
+
+	let urlDev = window.location.href;
+	urlDev = urlDev.split('/index')[0];
+	const path = '/my-tag/';
 
 	const myTag = document.getElementsByTagName('my-tag')[0];
-	// const myTagModule = document.getElementById('my-tag').load('my-tag.html');
+
+	console.log(urlDev + path + 'my-tag.html');
 
 	var xhr= new XMLHttpRequest();
-	xhr.open('GET', urlBase + 'contato.html', true);
+	xhr.open('GET', urlDev + path + 'my-tag.html', true);
 	xhr.onreadystatechange= function() {
 	    if (this.readyState!==4) return;
-	    if (this.status!==200) return; // or whatever error handling you want
+	    if (this.status!==200) return;
 	    document.getElementsByTagName('my-tag')[0].innerHTML= this.responseText;
 	};
 	xhr.send();
 
-	console.log(myTag);
-	// console.log(myTagModule);
 })();
