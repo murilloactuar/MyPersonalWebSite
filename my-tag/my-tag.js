4d50ba8b78cf0f6fab3cdf2142fa77eb3e1c4910
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2018-03-13 16:03:58 -0300

    Update my-tag.js

--- my-tag/my-tag.js
+++ my-tag/my-tag.js
@@ -1,22 +1,5 @@
 (function () {
 
-	// const urlProd = 'https://murilloves.github.io/';
-
-	let urlDev = window.location.href;
-	urlDev = urlDev.split('/index')[0];
-	const path = '/my-tag/';
-
-	const myTag = document.getElementsByTagName('my-tag')[0];
-
-	console.log(urlDev + path + 'my-tag.html');
-
-	var xhr= new XMLHttpRequest();
-	xhr.open('GET', urlDev + path + 'my-tag.html', true);
-	xhr.onreadystatechange= function() {
-	    if (this.readyState!==4) return;
-	    if (this.status!==200) return;
-	    document.getElementsByTagName('my-tag')[0].innerHTML= this.responseText;
-	};
-	xhr.send();
+	appendTags('my-tag');
 
 })();
