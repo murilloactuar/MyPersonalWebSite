c4de54b2f165ab00b4e35b58536f20cfefd27592
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2018-03-13 14:43:56 -0300

    Update your-tag.js

--- your-tag/your-tag.js
+++ your-tag/your-tag.js
@@ -1,1 +1,20 @@
+(function () {
 
+	let urlDev = window.location.href;
+	urlDev = urlDev.split('/index')[0];
+	const path = '/your-tag/';
+
+	const yourTag = document.getElementsByTagName('your-tag')[0];
+
+	console.log(urlDev + path + 'your-tag.html');
+
+	var xhr= new XMLHttpRequest();
+	xhr.open('GET', urlDev + path + 'your-tag.html', true);
+	xhr.onreadystatechange= function() {
+	    if (this.readyState!==4) return;
+	    if (this.status!==200) return;
+	    document.getElementsByTagName('your-tag')[0].innerHTML= this.responseText;
+	};
+	xhr.send();
+
+})();
