e4b74f5e0fdd5ffa84fb622d716a64a923283045
Murillo Vieira e Silva <murillo.vieiraesilva@gmail.com>
2018-03-13 17:03:38 -0300

    Update imports.js

--- imports.js
+++ imports.js
@@ -8,11 +8,14 @@ const paths = [
 	['your-tag/your-tag.js'],
 ];
 
+let scriptIndex = 0;
+
 (function includeHTML() {
 
 	function insertScript(source, callback) {
 		let script = document.createElement('script');
-		let prior = document.getElementsByTagName('script')[0];
+		let prior = document.getElementsByTagName('script')[scriptIndex];
+		scriptIndex++;
 		script.async = 1;
 
 		script.onload = script.onreadystatechange = function( _, isAbort ) {
@@ -31,15 +34,15 @@ const paths = [
 
 		script.src = source;
 		prior.parentNode.insertBefore(script, prior);
-		console.log('innerCallback', source);
+		// console.log('innerCallback', source);
 		callback;
 	}
 
 	function importSources(sources, callback) {
 		for (let source of sources) {
-			insertScript(source, callback);
+			insertScript(source, null);
 		}
-		console.log('outterCallback', sources);
+		// console.log('outterCallback', sources);
 		callback;
 	}
 
